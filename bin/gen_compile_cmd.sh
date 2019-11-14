a1af8b0b508bed4544463e523a0fc7ddc6810f16
wansong <betterofsong@gmail.com>
2019-11-15 00:51:37 +0800

    Ignore xcodebuild clean error

--- bin/gen_compile_cmd.sh
+++ bin/gen_compile_cmd.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+set -e
 
 pod install
 xcodebuild -workspace VimIdeDemo.xcworkspace/ -list -json | jq -r  '.workspace.schemes | .[]' | \
   while read scheme ; \
-    do xcodebuild clean -workspace VimIdeDemo.xcworkspace/ -scheme $scheme ; \
+    do xcodebuild clean -workspace VimIdeDemo.xcworkspace/ -scheme $scheme || true ; \
     done ; \
 xcodebuild -workspace VimIdeDemo.xcworkspace/ -scheme VimIdeDemo | xcpretty --report json-compilation-database  --output compile_commands.json
